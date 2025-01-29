20d88d046dc67704cc988b50348fab0f3a2931d2
Abiy Addis <abiymw17@gmail.com>
2025-01-29 17:30:11 +0300

    Day 3

---
+++ Day 3/205. Isomorphic String(LeetCode).py
@@ -0,0 +1,15 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        dic = {}
+        ms = set()
+        for i in range(len(s)):
+            if s[i] in dic:
+                if dic[s[i]] != t[i]:
+                    return False
+
+            else:
+                if t[i] in ms:
+                    return False
+                ms.add(t[i])
+                dic[s[i]] = t[i]
+        return True
