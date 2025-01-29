aaaf7503c0f840035e0c8699a7b14c2ed79db847
Abiy Addis <abiymw17@gmail.com>
2025-01-29 17:39:35 +0300

    Day 2 better version

--- Day 2/735. Asteroid Collision(LeetCode).py
+++ Day 2/735. Asteroid Collision(LeetCode).py
@@ -1,15 +1,15 @@
+
 class Solution:
     def asteroidCollision(self, ast: List[int]) -> List[int]:
         res = []
-        for a in ast:
-            if a > 0:
-                res.append(a)
+        for i,n in enumerate(ast):
+            if n > 0:
+                res.append(n)
             else:
-                while res and res[-1] > 0 and res[-1] < -a:
+                while res and res[-1] > 0 and res[-1] < -n:
                     res.pop()
                 if not res or res[-1] < 0:
-                    res.append(a)
-                elif res[-1] == -a:
+                    res.append(n)
+                elif res[-1] == -n:
                     res.pop()
-
         return res
