aab097003acef1491ee9079c2bba62680bdf4e19
Abiy Addis <abiymw17@gmail.com>
2025-02-05 23:59:34 +0300

    Week 2 Day 3 Problem

---
+++ Week 2/Day 3/1790. Check if One String Swap Can Make Strings Equal.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        size = len(s1)
+        count = 0
+        dic1 = Counter(s1)
+        dic2 = Counter(s2)
+        for d in dic1:
+            if d not in dic2:
+                return False
+            if dic1[d] != dic2[d]:
+                return False
+        for i in range(size):
+            if s1[i] != s2[i]:
+                count += 1
+            if count == 3:
+                return False
+        return True
