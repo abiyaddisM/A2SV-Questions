ca727cf04c127b349f149e6d8e8a8065f6be6bab
Abiy Addis <abiymw17@gmail.com>
2025-03-09 00:05:11 +0300

    Week 6 Day 6 contest

---
+++ Week 6/Day 6/Codeforces - A2SV Education Phase I - Contest 6/E. Straw Hat's Blue-Red Permutation.py
@@ -0,0 +1,53 @@
+from collections import Counter
+import math
+def INT():
+    return int(input())
+def IA():
+    return list(map(int,input().strip().split()))
+def SA():
+    return input().split()
+def STR():
+    return input()
+def PA(arr):
+    print(' '.join(map(str, arr)))
+
+t = INT()
+for _ in range(t):
+    size = INT()
+    num = IA()
+    color = STR()
+    red = [0 for i in range(size)]
+    blue = [0 for i in range(size)]
+    state = True
+    for i in range(size):
+        if color[i] == 'R':
+            if num[i] > size:
+                state = False
+                break
+            v = max(num[i] - 1,0)
+            red[v] += 1
+        else:
+            if num[i] < 1:
+                state = False
+                break
+            v = min(num[i], size)
+            blue[v - 1] += 1
+    for i in range(1,size):
+        blue[i] += blue[i - 1]
+        red[-(i + 1)] += red[-i]
+
+    for i in range(size):
+        if size + 1 < (i + 1) + red[i]:
+            state = False
+            break
+    for i in range(size):
+        if blue[i] > i + 1:
+            state = False
+            break
+    if state:
+        print('YES')
+    else:
+        print('NO')
+
+
+
