1a4aa5fb90ffae7c31bf73d2b57dea9a2fdfa615
Abiy Addis <abiymw17@gmail.com>
2025-03-11 22:59:57 +0300

    Week 7 Day 2 Problem

---
+++ Week 7/Day 2/Codeforces - ITMO Academy pilot course - Two Pointers Method - Step 1/A. Merging Arrays.py
@@ -0,0 +1,27 @@
+from collections import Counter
+import math
+def INT():
+    return int(input())
+def IA():
+    return list(map(int,input().strip().split()))
+def SA():
+    return input().split()
+def STR():
+    return input()
+def PA(arr):
+    print(' '.join(map(str, arr)))
+
+
+n, m = IA()
+a1 = IA()
+a2 = IA()
+res = []
+l, r = 0, 0
+while l < len(a1) or r < len(a2):
+     if l > len(a1) or (r < len(a2) and a1[l] > a2[r]):
+          res.append(a2[r])
+          r += 1
+     else:
+          res.append(a1[l])
+          l += 1
+PA(res)
