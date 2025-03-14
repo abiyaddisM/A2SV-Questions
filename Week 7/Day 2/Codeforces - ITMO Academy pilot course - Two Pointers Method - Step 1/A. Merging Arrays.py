c6c7ff3ba30d269128cd3a6623ffa1fe2a2cfcca
Abiy Addis <abiymw17@gmail.com>
2025-03-14 22:58:06 +0300

    Week 7 Day 5 Problem

--- Week 7/Day 2/Codeforces - ITMO Academy pilot course - Two Pointers Method - Step 1/A. Merging Arrays.py
+++ Week 7/Day 2/Codeforces - ITMO Academy pilot course - Two Pointers Method - Step 1/A. Merging Arrays.py
@@ -18,10 +18,18 @@ a2 = IA()
 res = []
 l, r = 0, 0
 while l < len(a1) or r < len(a2):
-     if l > len(a1) or (r < len(a2) and a1[l] > a2[r]):
-          res.append(a2[r])
-          r += 1
-     else:
-          res.append(a1[l])
-          l += 1
+    if l < len(a1) and r < len(a2):
+         if a1[l] > a2[r]:
+              res.append(a2[r])
+              r += 1
+         else:
+              res.append(a1[l])
+              l += 1
+    elif l < len(a1):
+        res.append(a1[l])
+        l += 1
+    else:
+        res.append(a2[r])
+        r += 1
+
 PA(res)
