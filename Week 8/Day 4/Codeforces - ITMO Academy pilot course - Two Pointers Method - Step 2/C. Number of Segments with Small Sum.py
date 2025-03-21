6fc07712f3770fe24879d51a0915d74511d6ed8b
Abiy Addis <abiymw17@gmail.com>
2025-03-21 16:02:03 +0300

    Week 8 Day 4 Problem

---
+++ Week 8/Day 4/Codeforces - ITMO Academy pilot course - Two Pointers Method - Step 2/C. Number of Segments with Small Sum.py
@@ -0,0 +1,25 @@
+from collections import Counter
+import math
+def INT():
+    return int(input())
+def IA():
+    return list(map(int,input().strip().split()))
+def SA():
+    return input().split()
+def STR():
+    return input()
+def PA(arr):
+    print(' '.join(map(str, arr)))
+
+size, s = IA()
+nums = IA()
+res = 0
+total = 0
+l = 0
+for r in range(size):
+    total += nums[r]
+    while total > s:
+        total -= nums[l]
+        l += 1
+    res += (r - l) + 1
+print(res)
