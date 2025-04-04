88a051e946cbbf3053c02c5aa152f2a1d1c5a39c
Abiy Addis <abiymw17@gmail.com>
2025-04-04 13:38:08 +0300

    Week 10 Day 5 Problem

---
+++ Week 10/Day 5/632. Smallest Range Covering Elements from K Lists(LeetCode).py
@@ -0,0 +1,23 @@
+# April 4 2025
+# https://leetcode.com/problems/smallest-range-covering-elements-from-k-lists/description/
+import heapq
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        res = []
+        temp = [0 for _ in range(len(nums))]
+        heap = []
+        maxs = float('-inf')
+        for i in range(len(nums)):
+            heapq.heappush(heap, (nums[i][0],i, 0))
+            maxs = max(maxs, nums[i][0])
+
+        while True:
+            mins, i, j = heapq.heappop(heap)
+            if len(res) == 0 or res[1] - res[0] > maxs - mins:
+                res = [mins, maxs]
+            j += 1
+            if len(nums[i]) <= j :
+                break
+            heapq.heappush(heap, (nums[i][j],i, j))
+            maxs = max(maxs, nums[i][j])
+        return res
