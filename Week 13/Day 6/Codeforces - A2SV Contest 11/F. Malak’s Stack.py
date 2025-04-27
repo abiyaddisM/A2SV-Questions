743642a8526ece57ec73d224c197e3dcf9338376
Abiy Addis <abiymw17@gmail.com>
2025-04-27 10:51:02 +0300

    Week 13 Day 6 Contest

---
+++ Week 13/Day 6/Codeforces - A2SV Contest 11/F. Malak’s Stack.py
@@ -0,0 +1,20 @@
+from collections import Counter,deque
+import math
+def INT():
+    return int(input())
+def IA():
+    return list(map(int,input().strip().split()))
+def SA():
+    return input().split()
+def STR():
+    return input()
+def PA(arr):
+    print(' '.join(map(str, arr)))
+def YesNo(state):
+    if state:
+        print('YES')
+    else:
+        print('NO')
+
+size = INT()
+
