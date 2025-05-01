799da259a0fbccd27ac5205ca1ac7880667ec5ca
Abiy Addis <abiymw17@gmail.com>
2025-05-01 12:11:02 +0300

    Week 14 Day 4 Problem

---
+++ Week 14/Day 4/1922. Count Good Numbers(LeetCode).py
@@ -0,0 +1,15 @@
+# May 1 2025
+# https://leetcode.com/problems/count-good-numbers/description/
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        MOD = 10 ** 9 + 7
+        def helper(x,n):
+            if n == 0:
+                return 1
+            if n % 2 == 0:
+                t = helper(x, n // 2)
+                return (t * t) % MOD
+            return (helper(x,n - 1) * x) % MOD
+
+
+        return helper(5, math.ceil(n / 2)) * helper(4, math.floor(n / 2)) % MOD
