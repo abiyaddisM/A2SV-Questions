b1c2c3425b4c1ef6d7bd35c312519d432eb89d41
Abiy Addis <abiymw17@gmail.com>
2025-05-13 14:39:06 +0300

    Week 16 Day 1 Contest

---
+++ Codeforces - A2SV Contest 13/A. Can I Square.py
@@ -0,0 +1,26 @@
+from collections import Counter,deque
+import math
+def INT():
+    return int(input())
+def IA():
+    return list(map(int,input().strip().split()))
+def SA():
+    return input().split()
+def STR():
+    return input()
+def PA(arr):
+    print(' '.join(map(str, arr)))
+def YesNo(state):
+    if state:
+        print('YES')
+    else:
+        print('NO')
+
+
+t = INT()
+for _ in range(t):
+    size = INT()
+    nums = IA()
+    total = sum(nums)
+    sqr = math.isqrt(total)
+    YesNo(sqr ** 2 == total)
