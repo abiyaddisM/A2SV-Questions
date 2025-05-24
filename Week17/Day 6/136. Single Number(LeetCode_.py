a14be2b743f58a9d91c32ff36730356fef8a88a7
Abiy Addis <abiymw17@gmail.com>
2025-05-24 12:07:04 +0300

    Week 17 Day 6 Problem

---
+++ Week17/Day 6/136. Single Number(LeetCode_.py
@@ -0,0 +1,8 @@
+# May 24 2025
+# https://leetcode.com/problems/single-number/description/
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for n in nums:
+            res = n ^ res
+        return res
