703f1492030f0db4a1b2bcea44c2f253317d5dc0
Abiy Addis <abiymw17@gmail.com>
2025-05-29 19:15:57 +0300

    Week 18 Day 4 Problem

---
+++ Week 18/Day 4/98. Validate Binary Search Tree(LeetCode).py
@@ -0,0 +1,18 @@
+# May 29 2025
+# https://leetcode.com/problems/validate-binary-search-tree/description/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def helper(node, low, high):
+            if not node:
+                return True
+            if not (low < node.val < high):
+                return False
+            return helper(node.left, low, node.val) and helper(node.right, node.val, high)
+
+        return helper(root, float('-inf'), float('inf'))
