c7168117cf4206e22389d32a4d421ce48b097112
Abiy Addis <abiymw17@gmail.com>
2025-07-30 11:55:14 +0300

    Week 27 Day 3 Problem

---
+++ Week 27/Day 3/198. House Robber(LeetCode).py
@@ -0,0 +1,16 @@
+# July 30 2025
+# https://leetcode.com/problems/house-robber/description/
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dic = {}
+        def helper(i):
+            if i >= len(nums):
+                return 0
+            if i in dic:
+                return dic[i]
+            t1 = nums[i] + helper(i + 2)
+            t2 = helper(i + 1)
+            dic[i] = max(t1,t2)
+            return max(t1, t2)
+
+        return helper(0)    
