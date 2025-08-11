72593b35ca87f53f71a01444f00063529dab9ef1
Abiy Addis <abiymw17@gmail.com>
2025-08-11 20:51:19 +0300

    Week 29 Day 1 Problem

---
+++ Week 29/Day 1/392. Is Subsequence(LeetCode).py
@@ -0,0 +1,11 @@
+# August 11 2025
+# https://leetcode.com/problems/is-subsequence/description/
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        l = 0
+        r = 0
+        while l < len(s) and r < len(t):
+            if s[l] == t[r]:
+                l += 1
+            r += 1
+        return l == len(s)
