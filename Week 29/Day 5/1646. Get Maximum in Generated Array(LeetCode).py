0dce9571f1b2aa4b24b7c0a58c9b295b8297f273
Abiy Addis <abiymw17@gmail.com>
2025-08-15 20:51:49 +0300

    Week 29 Day 5 Problem

---
+++ Week 29/Day 5/1646. Get Maximum in Generated Array(LeetCode).py
@@ -0,0 +1,16 @@
+# August 15 2025
+# https://leetcode.com/problems/get-maximum-in-generated-array/description/
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        if n == 0:
+            return 0
+        nums = [0] * (n + 1)
+        nums[1] = 1
+
+        for i in range(1, n // 2 + 1):
+            if 2 * i <= n:
+                nums[2 * i] = nums[i]
+            if 2 * i + 1 <= n:
+                nums[2 * i + 1] = nums[i] + nums[i + 1]
+
+        return max(nums)
