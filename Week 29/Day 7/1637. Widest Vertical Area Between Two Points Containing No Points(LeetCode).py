dee417e81f69d77afe12cfde9ef4605db58b116c
Abiy Addis <abiymw17@gmail.com>
2025-08-17 23:06:00 +0300

    Week 29 Day 7 Problem

---
+++ Week 29/Day 7/1637. Widest Vertical Area Between Two Points Containing No Points(LeetCode).py
@@ -0,0 +1,9 @@
+# August 17 2025
+# https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/description/
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        points.sort(key= lambda x: x[0])
+        res = 0
+        for i in range(len(points) - 1):
+            res = max(res, points[i + 1][0] - points[i][0])
+        return res
