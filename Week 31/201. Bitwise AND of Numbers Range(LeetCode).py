f5aa0085222b34262fc35ce092ac6eb6e70289b6
Abiy Addis <abiymw17@gmail.com>
2025-08-25 22:15:32 +0300

    Week 31 Day 1 Problem

---
+++ Week 31/201. Bitwise AND of Numbers Range(LeetCode).py
@@ -0,0 +1,11 @@
+# August 25 2025
+# https://leetcode.com/problems/bitwise-and-of-numbers-range/description/
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        shift = 0
+        while left < right:
+            left >>= 1
+            right >>= 1
+            shift += 1
+        return left << shift
+
