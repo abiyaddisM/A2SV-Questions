359c319f5c4e58fed90c995e318b6972fa12852d
Abiy Addis <abiymw17@gmail.com>
2025-08-29 21:11:39 +0300

    Week 31 Day 5 Problem

---
+++ Week 31/Day 5/371. Sum of Two Integers(LeetCode).py
@@ -0,0 +1,12 @@
+# August 29 2025
+# https://leetcode.com/problems/sum-of-two-integers/description/
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        MASK = 0xFFFFFFFF
+        MAX_INT = 0x7FFFFFFF
+
+        while b != 0:
+            carry = (a & b) & MASK
+            a = (a ^ b) & MASK
+            b = (carry << 1) & MASK
+        return a if a <= MAX_INT else ~(a ^ MASK)
