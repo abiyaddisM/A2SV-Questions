291d57451388da4540ecd3bfe1cf5b4a8cb519d1
Abiy Addis <abiymw17@gmail.com>
2025-09-14 20:53:10 +0300

    Week 33 Day 7 Problem

---
+++ Week 33/Day 7/89. Gray Code(LeetCode).py
@@ -0,0 +1,5 @@
+# September 14 2025
+# https://leetcode.com/problems/gray-code/description/
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        return [i ^ (i >> 1) for i in range(1 << n)]
