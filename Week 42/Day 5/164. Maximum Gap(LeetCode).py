e720e2d698446258328ed8e7748f8328a34e8422
Abiy Addis <abiymw17@gmail.com>
2025-11-14 23:02:40 +0300

    Week 42 Day 5 Problem

---
+++ Week 42/Day 5/164. Maximum Gap(LeetCode).py
@@ -0,0 +1,9 @@
+# November 14 2025
+# https://leetcode.com/problems/maximum-gap/description/
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        nums.sort()
+        res = 0
+        for i in range(len(nums) - 1):
+            res = max(res, nums[i + 1] - nums[i])
+        return res
